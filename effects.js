e54896bca979ebb88cf39a9e7fa55e9d33d6ca30
Ian <iangaunt999@gmail.com>
2022-04-20 10:47:40 -0700

    v2.0.2
    Added on-scroll effect for header
    
    Small reformats: v2.0.2

--- effects.js
+++ effects.js
@@ -1,1 +1,23 @@
-console.log("fart"); 
+/*
+
+    Welcome to the big brains of the website.
+    JavaScript rocks! When it wants to. Sometimes it sucks.
+
+*/ 
+
+//-> Observer code for on-scroll animations. 
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        const waving = entry.target.querySelector('.waving-box')
+        
+        if (entry.isIntersecting) {
+            waving.classList.add("animate__rubberBand");
+            return; 
+        }
+
+        waving.classList.remove('animate__rubberBand');
+    });
+});
+
+observer.observe(document.querySelector('.text-section-wrapper'));
+
