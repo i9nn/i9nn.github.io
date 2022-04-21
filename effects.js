6e36f4370042dc7493fbcb27847b439659729abd
Ian <iangaunt999@gmail.com>
2022-04-21 14:23:01 -0700

    v2.0.3 merging of effects.js to main html

--- effects.js
+++ effects.js
@@ -5,19 +5,3 @@
 
 */ 
 
-//-> Observer code for on-scroll animations. 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        const waving = entry.target.querySelector('.waving-box')
-        
-        if (entry.isIntersecting) {
-            waving.classList.add("animate__rubberBand");
-            return; 
-        }
-
-        waving.classList.remove('animate__rubberBand');
-    });
-});
-
-observer.observe(document.querySelector('.text-section-wrapper'));
-
