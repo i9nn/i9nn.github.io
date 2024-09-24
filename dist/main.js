b03a97cb249728dbc3570aea6a4b975171ec43d5
Ian G. <iangaunt999@gmail.com>
2024-09-23 17:48:15 -0700

    Webpack rebuild

--- dist/main.js
+++ dist/main.js
@@ -33393,81 +33393,64 @@ if (false) {} else {
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
 
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 __webpack_require__(/*! ../css/style.css */ "./src/css/style.css");
-var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
-var client_1 = __webpack_require__(/*! react-dom/client */ "./node_modules/react-dom/client.js");
-(function () { return __awaiter(void 0, void 0, void 0, function () {
-    var res, data;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                console.log("gg");
-                return [4 /*yield*/, fetch("https://discord.com/api/guilds/1050460521201152100", {
-                        headers: {
-                            Authorization: "Bot MTA1MDQ2MTc4MjAxNzMxNDgzNg.GWGemI.KFfts1WsZS-VQ5XqdpvhVWqpdWWA4PhL3xA4Yw"
-                        }
-                    })];
-            case 1:
-                res = _a.sent();
-                return [4 /*yield*/, res.json()];
-            case 2:
-                data = _a.sent();
-                console.log(data);
-                return [2 /*return*/];
-        }
-    });
-}); })();
-// const img = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`
-function App() {
-    return (react_1.default.createElement("div", { className: "container" },
-        react_1.default.createElement("h1", null, "Ian's React Skeleton"),
-        react_1.default.createElement("h2", null,
-            "Feel free to begin editing ",
-            react_1.default.createElement("span", null, "index.tsx"),
-            ".")));
+const react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
+const client_1 = __webpack_require__(/*! react-dom/client */ "./node_modules/react-dom/client.js");
+function shorten(str, title) {
+    let bound = 20;
+    if (!title)
+        bound = 40;
+    return (str.length > bound) ? str.substring(0, bound + 1) + "..." : str;
 }
-var root = (0, client_1.createRoot)(document.getElementById("main"));
-root.render(react_1.default.createElement(App, null));
+function fetchStatus(status) {
+    if (status === "online") {
+        return "rgb(59, 165, 92)";
+    }
+    else if (status === "idle") {
+        return "rgb(250, 166, 26)";
+    }
+    else if (status === "dnd") {
+        return "rgb(237, 66, 69)";
+    }
+    else if (status === "offline") {
+        return "rgb(116, 127, 141)";
+    }
+}
+function Spotify(props) {
+    if (props.listening) {
+        let songName = json.data.spotify.song;
+        let artistName = json.data.spotify.artist + " • " + json.data.spotify.album;
+        return (react_1.default.createElement("div", { className: "spotify" },
+            react_1.default.createElement("img", { className: "cover", src: `${json.data.spotify.album_art_url}` }),
+            react_1.default.createElement("div", { className: "description" },
+                react_1.default.createElement("h1", null, shorten(songName, true)),
+                react_1.default.createElement("h2", null, shorten(artistName, false)))));
+    }
+}
+function App(props) {
+    return (react_1.default.createElement("div", { className: "container" },
+        react_1.default.createElement("div", { className: "profile" },
+            react_1.default.createElement("div", { className: "pfp" },
+                react_1.default.createElement("img", { src: `https://cdn.discordapp.com/avatars/${json.data.discord_user.id}/${json.data.discord_user.avatar}?size=1024` }),
+                react_1.default.createElement("div", { className: "circle", style: { backgroundColor: fetchStatus(json.data.discord_status) } })),
+            react_1.default.createElement("div", { className: "info" },
+                react_1.default.createElement("h1", null, `@${json.data.discord_user.username}`),
+                react_1.default.createElement("h2", null, "CS student, music producer, graphic designer"),
+                react_1.default.createElement(Spotify, { listening: json.data.listening_to_spotify })))));
+}
+let json = null;
+let root = (0, client_1.createRoot)(document.getElementById("main"));
+setInterval(() => {
+    (async () => {
+        const response = await fetch("https://api.lanyard.rest/v1/users/614954208139149319");
+        json = await response.json();
+        root.render(react_1.default.createElement(App, { json: json }));
+    })();
+}, 1000);
 
 
 /***/ })
