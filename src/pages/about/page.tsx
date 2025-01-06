02b39a70dea2312ccef0c1179a96563a90da4c3a
Ian G. <iangaunt999@gmail.com>
2025-01-06 08:36:38 -0800

    New albums for Jan 4 + Jan 5

--- src/pages/about/page.tsx
+++ src/pages/about/page.tsx
@@ -16,6 +16,8 @@ export default function About() {
 
 						<Album date="January 2" name="Pop 2" artist="Charli XCX" score={8} />
 						<Album date="January 3" name="Karma" artist="Pharaoh Sanders" score={9} />
+						<Album date="January 4" name="ロンググッドバイ" artist="Kinokoteikoku" score={8} />
+						<Album date="January 5" name="The Rockwood Escape Plan" artist="Devon Hendryx" score={7} />
 					</div>
 				</Template>
 			</main>
