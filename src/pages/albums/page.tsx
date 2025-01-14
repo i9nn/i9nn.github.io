c374036a72e15adfef8f87c77b09f44e3104890a
Ian G. <iangaunt999@gmail.com>
2025-01-13 19:38:30 -0800

    Added new albums

--- src/pages/albums/page.tsx
+++ src/pages/albums/page.tsx
@@ -18,6 +18,12 @@ export default function About() {
 						<Album date="January 3" name="Karma" artist="Pharaoh Sanders" score={9} />
 						<Album date="January 4" name="ロンググッドバイ" artist="Kinokoteikoku" score={8} />
 						<Album date="January 5" name="The Rockwood Escape Plan" artist="Devon Hendryx" score={7} />
+						<Album date="January 6" name="333" artist="Bladee" score={8} />
+						<Album date="January 7" name="Thembi" artist="Pharaoh Sanders" score={6} />
+						<Album date="January 8" name="channel orange" artist="Frank Ocean" score={5} />
+						<Album date="January 9" name="Yanqui U.X.O" artist="GY!BE" score={9} />
+						<Album date="January 10" name="Sleeping Beauty" artist="Sun Ra" score={9} />
+						<Album date="January 11" name="Structures from Silence" artist="Steve Roach" score={9} />
 					</div>
 				</Template>
 			</main>
